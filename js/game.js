cd51d78d218f4bfc8e3458c2a68238e500e1f1a0
Jake Bolton <ninmonkeys@gmail.com>
2018-06-12 16:44:07 -0500

    game fn skeleton

--- js/game.js
+++ js/game.js
@@ -1,1 +1,51 @@
 console.log("done");
+
+let canvas = null;
+let ctx = null;
+let visualDebug = true;
+let stats = new Stats();
+
+function drawShip() {
+    ctx.beginPath();
+    ctx.rect(100, 100, 100, 100);
+    ctx.fillStyle = "#ddd";
+    ctx.fill();
+    ctx.closePath();
+}
+
+function drawScore() {
+    ctx.font = "16px arial";
+    ctx.fillStyle = "#fff";
+    ctx.textAlign = 'left';
+    ctx.textBaseline = 'top';
+    ctx.fillText("Score: " + 1234, 10, 10);
+}
+
+function init() {
+    canvas = document.getElementById("canvas");
+    ctx = canvas.getContext("2d");
+    if(visualDebug) {
+        document.body.appendChild(stats.dom);
+        stats.showPanel(1);
+    }
+}
+
+function loop() {
+    let time = performance.now() / 1000;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    stats.begin();
+
+    drawShip();
+    drawScore();
+
+    stats.end();
+
+    requestAnimationFrame(loop);
+}
+
+// entry point
+document.addEventListener('DOMContentLoaded', function() {
+    init();
+    loop();
+}, false);
--- readme.md
+++ readme.md
@@ -1,3 +1,10 @@
 # about
 
-Asteroids clone without libraries, uses ES6 features.
+Asteroids clone without libraries, uses ES6 features. I  purposely don't use `class` keyword.
+
+# future
+
+- profile speed, only update dirty rects
+- use more canvases for performance such as ship/rock/bg.
+- Vector2d
+- auto-pause when changing focus on tab or windows?
